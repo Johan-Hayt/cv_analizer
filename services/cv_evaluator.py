bca87bbaa1e063ee13fb2deeb00b3e2c68853f95
Johan-Hayt <55248962+Johan-Hayt@users.noreply.github.com>
2025-12-24 17:48:25 -0500

    feat: Se crea la función para ejecutar el evaluador de cv. Se instancia el modelo llm y se declara la invcación del modelo.

---
+++ services/cv_evaluator.py
@@ -0,0 +1,46 @@
+from langchain_openai import ChatOpenAI
+from prompt.cv_prompts import crear_sistema_prompts
+from models.cv_model import AnalisisCv
+
+def crear_evaluador() -> AnalisisCv:
+    
+    #Instanciar modelo base
+    llm_base = ChatOpenAI(
+        model="gpt-5-nano",
+        temperature=0.2,
+    )
+
+    # Modelo con salida estructurada
+    llm_structured = llm_base.with_structured_output(AnalisisCv)
+
+    # plantilla prompt
+    chat_prompt = crear_sistema_prompts()
+
+    # pipeline del proceso
+    return chat_prompt | llm_structured
+
+
+def evaluador_cv(texto_cv: str, descripcion_puesto: str)->AnalisisCv:
+    try:
+        # instanciando al evaluador
+        evaluador = crear_evaluador()
+
+        # invocando al evaluador
+        return evaluador.invoke(
+                    {
+                        "texto_cv": texto_cv,
+                        "descripcion_puesto": descripcion_puesto,
+                    }
+                )
+
+    except:
+        return AnalisisCv(
+            nombre_candidato="Error en procesamiento.",
+            experiencia_anios=0,
+            habilidades_tecnicas=["Error al leer el PDF"],
+            education="",
+            experiencia_relevante="",
+            fortalezas=[""],
+            areas_mejora=[""],
+            porcentaje_adecuacion=0,
+        )
